4bbd9b3caa76d73fec62ca3ff8e3a99f7b28170a
wanyki <wanyki@126.com>
2026-01-18 21:32:07 +0800

    修改add-cards.js

--- api/add-cards.js
+++ api/add-cards.js
@@ -1,6 +1,16 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
+    // 在 handler 函数的最上方添加
+res.setHeader('Access-Control-Allow-Credentials', true);
+res.setHeader('Access-Control-Allow-Origin', '*'); 
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+}
+
     if (req.method !== 'POST') {
         return res.status(405).json({ success: false, message: '必须使用 POST 请求' });
     }
