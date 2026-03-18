cce4c4edce2570bf2853b21f4ba13236f58e5bb4
wanyki <wanyki@126.com>
2026-03-18 16:41:28 +0800

    修改国家映射

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ if (document.getElementById('app')) {
             countryMap: { 
                 "中国": "China", "日本": "Japan", "美国": "United States", 
                 "德国": "Germany", "英国": "United Kingdom", "法国": "France", 
-                "韩国": "Korea", "俄罗斯": "Russia", "加拿大": "Canada", "澳大利亚": "Australia"
+                "韩国": "Korea", "俄罗斯": "Russia", "加拿大": "Canada", "澳大利亚": "Australia",
+                "泰国": "Thailand", "意大利": "Italy", "西班牙": "Spain", "荷兰": "Netherlands"
             }
         },
         computed: {
